5495d7d716508a65e4c37beae3f91261ccf14407
Naman Dhariwal <114096680+NamanDhariwal@users.noreply.github.com>
2025-11-17 10:51:51 -0500

    Update distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -1,10 +1,6 @@
 #include <RcppArmadillo.h>
 using namespace Rcpp;
 
-// [[Rcpp::depends(RcppArmadillo)]]
-
-// x: n x p, y: m x p; returns n x m distance matrix (Euclidean)
-// [[Rcpp::export]]
 arma::mat euclidean_distance(const arma::mat& x, const arma::mat& y) {
   arma::mat out(x.n_rows, y.n_rows, arma::fill::zeros);
   for (arma::uword i = 0; i < x.n_rows; ++i) {
